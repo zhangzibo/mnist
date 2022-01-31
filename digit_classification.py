588ddf9a98bb881c58eb1966e8aa83c99b42e8ae
zhangzibo <zhangzhangzibozibo@gmail.com>
2022-01-30 17:01:22 -0800

    read and visualize file,

--- digit_classification.py
+++ digit_classification.py
@@ -1,28 +1,39 @@
 import struct
-import numpy as numpy
-import matplotlib as plt
+import numpy as np
+import matplotlib.pyplot as plt
 import os
-
 
 def load_data():
     with open("train-labels-idx1-ubyte", "rb") as labels:
         magic, n = struct.unpack(">II", labels.read(8))
         train_labels = np.fromfile(labels, dtype=np.uint8)
 
+    with open("train-images-idx3-ubyte", "rb") as imgs:
+        magic, num, nrows, ncols = struct.unpack(">IIII", imgs.read(16))
 
-# import gzip
-# f = gzip.open('train-images-idx3-ubyte.gz','r')
+        train_images = np.fromfile(imgs, dtype= np.uint8).reshape(num,784)  
+        #784 is becasue each picture of the digit is 28 pixel * 28pixel need to flatten it out
 
-# image_size = 28
-# num_images = 5
+    with open("t10k-labels-idx1-ubyte", "rb") as labels:
+        magic, n = struct.unpack(">II", labels.read(8))
+        test_labels = np.fromfile(labels, dtype=np.uint8)
 
-# import numpy as np
-# f.read(16)
-# buf = f.read(image_size * image_size * num_images)
-# data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-# data = data.reshape(num_images, image_size, image_size, 1)
+    with open("t10k-images-idx3-ubyte", "rb") as imgs:
+        magic, num, nrows, ncols = struct.unpack(">IIII", imgs.read(16))
+        test_images = np.fromfile(imgs, dtype= np.uint8).reshape(num,784)  
 
-# import matplotlib.pyplot as plt
-# image = np.asarray(data[2]).squeeze()
-# plt.imshow(image)
-# plt.show()
+    return train_images, train_labels, test_images, test_labels
+
+
+def vistualize_data(img_array, label_array):
+    fig, ax = plt.subplots(nrows=10,ncols=10,sharex=True, sharey=True)
+    ax= ax.flatten()
+
+    for i in range(100):
+        img = img_array[label_array][i].reshape(28,28)
+        ax[i].imshow(img,cmap="Greys", interpolation="nearest")
+    plt.show()
+
+train_x,train_y, test_x,test_y =load_data()
+
+vistualize_data(train_x,train_y)
